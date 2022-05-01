94c67a2f4037401b42ac6ba96dac495c33f964eb
UwuigbeOshborn <101250802+UwuigbeOshborn@users.noreply.github.com>
2022-05-01 04:10:29 -0700

    Add files via upload

---
+++ Question 1.cs
@@ -0,0 +1,116 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("------------------------------    CypherCresentClass17 Assignment Question  1      ------------------------------");
+try
+{
+    //Creating an assignment folder
+    var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+    var myAssignment = Path.Combine(path, "Oshborn Assignment");
+    if (!Directory.Exists(myAssignment))
+    {
+        Directory.CreateDirectory(Path.Combine(path, "Oshborn Assignment"));
+        Console.WriteLine("Folder has been created");
+    }
+    else
+    {
+        Console.WriteLine("Folder Already Exists");
+    }
+
+    //Creating an assignment file within assignment folder
+    if (File.Exists(Path.Combine(myAssignment, "AssignmentModel1.csv")))
+    {
+        Console.WriteLine("File Already Exists");
+    }
+    else
+    {
+        File.Create(Path.Combine(myAssignment, "AssignmentModel1.csv"));
+        Console.WriteLine("File has been created");
+    }
+
+    //Writting into the file for AssignmentModel1
+    var line1 = new AssignmentModel1
+    {
+        IdLabel = "Id",
+        AssignmentName = "AssignmentName",
+        AssignmentLocation = "AssignmentLocation",
+        AssignmentModel2 = new AssignmentModel2()
+    };
+    var line2 = new AssignmentModel1
+    {
+        Id = 1,
+        AssignmentName = "Model1",
+        AssignmentLocation = Path.Combine(myAssignment, "AssignmentModel1.csv "),
+        AssignmentModel2 = new AssignmentModel2()
+    };
+    File.AppendAllText(Path.Combine(myAssignment, "AssignmentModel1.csv "), $"{line1.IdLabel},{line1.AssignmentName},{line1.AssignmentLocation},{line1.AssignmentModel2}\n");
+    File.AppendAllText(Path.Combine(myAssignment, "AssignmentModel1.csv "), $"{line2.Id},{line2.AssignmentName},{line2.AssignmentLocation},{line2.AssignmentModel2}\n\n\n");
+
+    //Writting into the file for AssignmentModel2
+    var line3 = new AssignmentModel2
+    {
+        IdLabel = "Id",
+        AssignmentName = "AssignmentName",
+        AssignmentLocation = "AssignmentLocation",
+        AssignmentModel3 = new AssignmentModel3()
+    };
+    var line4 = new AssignmentModel2
+    {
+        Id = 1,
+        AssignmentName = "Model2",
+        AssignmentLocation = Path.Combine(myAssignment, "AssignmentModel1.csv "),
+        AssignmentModel3 = new AssignmentModel3()
+    };
+    File.AppendAllText(Path.Combine(myAssignment, "AssignmentModel1.csv "), $"{line3.IdLabel},{line3.AssignmentName},{line3.AssignmentLocation},{line3.AssignmentModel3}\n");
+    File.AppendAllText(Path.Combine(myAssignment, "AssignmentModel1.csv "), $"{line4.Id},{line4.AssignmentName},{line4.AssignmentLocation},{line4.AssignmentModel3}\n\n\n");
+
+
+    //Writting into the file for AssignmentModel3
+    var line5 = new AssignmentModel3
+    {
+        IdLabel = "Id",
+        AssignmentName = "AssignmentName",
+        AssignmentLocation = "AssignmentLocation",
+    };
+    var line6 = new AssignmentModel3
+    {
+        Id = 1,
+        AssignmentName = "Model3",
+        AssignmentLocation = Path.Combine(myAssignment, "AssignmentModel1.csv "),
+    };
+    File.AppendAllText(Path.Combine(myAssignment, "AssignmentModel1.csv "), $"{line5.IdLabel},{line5.AssignmentName},{line5.AssignmentLocation}\n");
+    File.AppendAllText(Path.Combine(myAssignment, "AssignmentModel1.csv "), $"{line6.Id},{line6.AssignmentName},{line6.AssignmentLocation}\n\n\n");
+    Console.WriteLine("Successfully written to file, please check the file now");
+}
+catch (Exception ex)  //catching a IOException
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Please run the program again, in order to write to file");
+}
+
+
+public class AssignmentModel1
+{
+    public int Id { get; set; }
+    public string IdLabel { get; set; }
+    public string AssignmentName { get; set; }
+    public string AssignmentLocation { get; set; }
+    public AssignmentModel2 AssignmentModel2 { get; set; }
+}
+public class AssignmentModel2
+{
+    public int Id { get; set; }
+    public string IdLabel { get; set; }
+    public string AssignmentName { get; set; }
+    public string AssignmentLocation { get; set; }
+    public AssignmentModel3 AssignmentModel3 { get; set; }
+
+}
+public class AssignmentModel3
+{
+    public int Id { get; set; }
+    public string IdLabel { get; set; }
+    public string AssignmentName { get; set; }
+    public string AssignmentLocation { get; set; }
+
+}
---
+++ Question 2.cs
@@ -0,0 +1,120 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("------------------------------      CypherCresentClass17 Assignment Question 2      ------------------------------");
+
+
+//Creating an assignment folder
+var path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+var myAssignment = Path.Combine(path, "Oshborn Assignment");
+if (!Directory.Exists(myAssignment))
+{
+    Directory.CreateDirectory(Path.Combine(path, "Oshborn Assignment"));
+    Console.WriteLine("Folder has been created");
+}
+else
+{
+    Console.WriteLine("Folder Already Exists");
+}
+
+//verifing if the sampledata.csv file is in the required directory, if not Search the system for such file and copy it to the required directory
+if (File.Exists(Path.Combine(myAssignment, "sampledata.csv")))
+{
+    Console.WriteLine("Good, the file Exists");
+}
+//else
+//{
+//    //File.Create(Path.Combine(myAssignment, "sampledata.csv"));
+//    Console.WriteLine("Note: in order to run the remaining lines of code, the sampledata.csv file to be converted to C# model should be copied to this directory => ...Desktop/Oshborn Assignment");
+//}
+else
+{
+    try
+    {
+        var fileSearch = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "*sampledata.csv*", SearchOption.AllDirectories);
+        File.Copy(Path.GetFullPath(fileSearch.First()), Path.Combine(myAssignment, "sampledata.csv"));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        try
+        {
+            var fileSearch = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "*sampledata.csv*", SearchOption.AllDirectories);
+            File.Copy(Path.GetFullPath(fileSearch.First()), Path.Combine(myAssignment, "sampledata.csv"));
+        }
+        catch (Exception)
+        {
+            Console.WriteLine(ex.Message);
+            try
+            {
+                var fileSearch = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic), "*sampledata.csv*", SearchOption.AllDirectories);
+                File.Copy(Path.GetFullPath(fileSearch.First()), Path.Combine(myAssignment, "sampledata.csv"));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(ex.Message);
+                try
+                {
+                    var fileSearch = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "*sampledata.csv*", SearchOption.AllDirectories);
+                    File.Copy(Path.GetFullPath(fileSearch.First()), Path.Combine(myAssignment, "sampledata.csv"));
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(ex.Message);
+                    try
+                    {
+                        var fileSearch = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos), "*sampledata.csv*", SearchOption.AllDirectories);
+                        File.Copy(Path.GetFullPath(fileSearch.First()), Path.Combine(myAssignment, "sampledata.csv"));
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine("Hello, the sampledata.csv file you intend to work on, can not be found on your system.");
+                    }
+                }
+            }
+        }
+    }
+}
+
+try
+{
+    //Note: in order to run the remaining lines of code, the sampledata.csv file to be converted to a C# model should be copied to this directory => ...Desktop/Oshborn Assignment
+    var info = new FileInfo(Path.Combine(myAssignment, "sampledata.csv"));
+    var sampleData = new List<SampleData>();
+    var csv_file_lines = File.ReadAllLines(Path.Combine(myAssignment, "sampledata.csv "));
+    foreach (var item in csv_file_lines)
+    {
+        var item_split = item.Split(",");
+        sampleData.Add(new SampleData
+        {
+            OrderDate = item_split[0],
+            Region = item_split[1],
+            Rep = item_split[2],
+            Item = item_split[3],
+            Units = item_split[4],
+            UnitCost = item_split[5],
+            Total = item_split[6]
+        });
+    }
+    //Checking the number of lines in the file
+    Console.WriteLine(sampleData.Count());
+
+    //Printing out each lines in the file
+    sampleData.ForEach(x => Console.WriteLine($"{x.OrderDate}, {x.Region}, {x.Rep}, {x.Item}, {x.Units}, {x.UnitCost}, {x.Total}"));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Please, make the file available and try again later");
+}
+
+public class SampleData
+{
+    public string OrderDate { get; set; }
+    public string Region { get; set; }
+    public string Rep { get; set; }
+    public string Item { get; set; }
+    public string Units { get; set; }
+    public string UnitCost { get; set; }
+    public string Total { get; set; }
+}
